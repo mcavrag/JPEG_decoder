b351876b9db87935a81217fb2f980158f6addb8b
Matija Čavrag <matija.cavrag.ext@ses-imagotag.com>
2020-07-24 19:18:06 +0200

    - Added: merged whole project from old repo

---
+++ JPEG_decoder_v3.c
@@ -0,0 +1,676 @@
+// Implementation of a simple JPEG decoder
+// Floating point implementation of IDCT, DEQUANT,... 
+// Matija Čavrag, FER, 2016, matija.cavrag@fer.hr
+// version:     3.0         10.6.2016.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <arm_neon.h>
+
+#define EOI1 0xff
+#define EOI2 0xd9
+#define SOS1 0xff
+#define SOS2 0xda
+
+static const unsigned int q_lum[8][8] = {
+
+{ 8, 6, 5, 8, 12, 20, 26, 31 }, { 6, 6, 7, 10, 13, 29, 30, 28 }, { 7, 7, 8, 12,
+		20, 29, 35, 28 }, { 7, 9, 11, 15, 26, 44, 40, 31 }, { 9, 11, 19, 28, 34,
+		55, 52, 39 }, { 12, 18, 28, 32, 41, 52, 57, 46 }, { 25, 32, 39, 44, 52,
+		61, 60, 51 }, { 36, 46, 48, 49, 56, 50, 52, 50 } };
+
+static const unsigned int q_chrom[8][8] = { { 9, 9, 12, 24, 50, 50, 50, 50 }, {
+		9, 11, 13, 33, 50, 50, 50, 50 }, { 12, 13, 28, 50, 50, 50, 50, 50 }, {
+		24, 33, 50, 50, 50, 50, 50, 50 }, { 50, 50, 50, 50, 50, 50, 50, 50 }, {
+		50, 50, 50, 50, 50, 50, 50, 50 }, { 50, 50, 50, 50, 50, 50, 50, 50 }, {
+		50, 50, 50, 50, 50, 50, 50, 50 }, };
+
+float32_t IDCTcoeff[8][8][8][8];
+
+FILE *fin, *fp, *fout;
+
+char input[100 * 100 * 100 * 100] = { 0 };
+
+int dc_lum[12][2], ac_lum[16][11][2];
+int dc_chrom[12][2], ac_chrom[16][11][2];
+
+int count = 0;
+
+int rl;
+
+int DCvalue[3] = { 0 };
+
+int dataBlock[8][8] = { { 0 } };
+
+int idctDataBlock[8][8] = { { 0 } };
+
+int DQFBlock[8][8] = { { 0 } };
+
+int DRLBlock[64] = { 0 };
+
+int DZZBlock[64] = { 0 };
+
+float IDCTBlock[8][8] = { { 0.0 } };
+
+float32_t YUV[8][8][3] = { { { 0 } } };
+
+uint32_t RGB[8][8][3] = { { { 0 } } };
+
+int fileoffset;
+
+char fname[128];
+
+unsigned char outputBuf[3 * 512 * 512] = { 0 };
+
+int getDCvalue(int * pos, int comp) {
+
+	int flag, i, j, temp;
+
+	int relPos, length;
+
+	int mask, shift, huffData;
+
+	//searching in huffman table for DC Lum or Chrom
+
+	for (i = 0; i < 12; i++) {
+
+		flag = 1;
+
+		mask = (comp == 1) ?
+				((1 << dc_lum[i][0]) - 1) : ((1 << dc_chrom[i][0]) - 1);
+		shift = (comp == 1) ? (dc_lum[i][0] - 1) : (dc_chrom[i][0] - 1);
+		huffData = (comp == 1) ? (dc_lum[i][1]) : (dc_chrom[i][1]);
+
+		relPos = 0;
+
+		length = (comp == 1) ? (dc_lum[i][0]) : (dc_chrom[i][0]);
+
+		for (j = length; j > 0; j--) {
+
+			if (((huffData & mask) >> shift) != (input[*pos + relPos] - 48)) {
+				flag = 0;
+				break;
+			} else {
+				relPos++;
+				shift -= 1;
+				mask = mask >> 1;
+			}
+		}
+
+		if (flag == 1) {
+			break;
+		}
+
+	}
+
+	*pos += relPos;
+
+	temp = 0;
+
+	for (j = 0; j < i; j++) {
+		temp *= 2;
+		if (input[*pos + j] == '1') {
+			temp++;
+		}
+	}
+
+	if (input[*pos] == '0') {
+		temp = temp + 1 - (int) pow(2, i);
+	}
+
+	*pos += i;
+
+	return temp;
+
+}
+
+void getACvalue(int *pos, int comp) {
+
+	int flag, i, j, k, temp, n;
+
+	int relPos, length;
+
+	int mask, shift, huffData;
+
+	int ACcounter = 1;
+
+	while (ACcounter <= 64) {
+
+		for (i = 0; i < 16; i++) {
+
+			for (j = 0; j < 11; j++) {
+
+				flag = 1;
+
+				mask = (comp == 1) ?
+						((1 << ac_lum[i][j][0]) - 1) :
+						((1 << ac_chrom[i][j][0]) - 1);
+				shift = (comp == 1) ?
+						(ac_lum[i][j][0] - 1) : (ac_chrom[i][j][0] - 1);
+				huffData =
+						(comp == 1) ? (ac_lum[i][j][1]) : (ac_chrom[i][j][1]);
+
+				relPos = 0;
+
+				length = (comp == 1) ? (ac_lum[i][j][0]) : (ac_chrom[i][j][0]);
+
+				for (k = length; k > 0; k--) {
+
+					if (((huffData & mask) >> shift)
+							!= (input[*pos + relPos] - 48)) {
+						flag = 0;
+						break;
+					} else {
+						relPos++;
+						shift -= 1;
+						mask = mask >> 1;
+					}
+				}
+
+				if (input[*pos + relPos + 1] == '\0') {
+					*pos += relPos;
+					DRLBlock[rl++] = 0;
+					DRLBlock[rl++] = 0;
+					return;
+				}
+
+				if ((flag == 1) && !(i != 15 && i != 0 && j == 0)) {
+
+					*pos += relPos;
+
+					if ((ACcounter + i + 1) > 64) {
+						DRLBlock[rl++] = 0;
+						DRLBlock[rl++] = 0;
+						return;
+					}
+
+					if (i == 0 && j == 0) {
+						DRLBlock[rl++] = 0;
+						DRLBlock[rl++] = 0;
+
+						return;
+					}
+
+					n = i;
+					temp = 0;
+
+					if (j == 0) {
+						DRLBlock[rl++] = n;
+						DRLBlock[rl++] = temp;
+
+					} else {
+
+						for (k = 0; k < j; k++) {
+							temp *= 2;
+
+							if (input[*pos + k] == '1') {
+								temp += 1;
+							}
+						}
+
+						if (input[*pos] == '0')
+							temp = temp + 1 - (int) pow(2, j);
+
+						*pos += j;
+
+						DRLBlock[rl++] = n;
+						DRLBlock[rl++] = temp;
+
+					}
+
+					ACcounter += i + 1;
+
+					break;
+
+				}
+
+				if (ACcounter >= 64) {
+					return;
+				}
+
+				flag = 1;
+			}
+		}
+
+	}
+}
+
+void DRLE() {
+	int rld = 1;
+	int i = 1;
+	int k = 0;
+	DZZBlock[0] = DRLBlock[0];
+	while (i < 64) {
+		if (DRLBlock[rld] == 0 && DRLBlock[rld + 1] == 0) {
+			for (k = i; k < 64; k++)
+				DZZBlock[k] = 0;
+			return;
+		}
+		for (k = 0; k < DRLBlock[rld]; k++) {
+			DZZBlock[i++] = 0;
+		}
+		if (i <= 63 && rld <= 62) {
+			DZZBlock[i++] = DRLBlock[rld + 1];
+			rld += 2;
+		} else if (i == 63 && rld <= 62) {
+			DZZBlock[i++] = DRLBlock[rld + 1];
+		} else if (i <= 63 && rld == 63) {
+			DZZBlock[i++] = 0;
+		} else if (i == 63 && rld == 63) {
+			DZZBlock[i] = DRLBlock[rld];
+		}
+	}
+}
+
+void DZigZag() {
+	DQFBlock[0][0] = DZZBlock[0];
+	DQFBlock[0][1] = DZZBlock[1];
+	DQFBlock[0][2] = DZZBlock[5];
+	DQFBlock[0][3] = DZZBlock[6];
+	DQFBlock[0][4] = DZZBlock[14];
+	DQFBlock[0][5] = DZZBlock[15];
+	DQFBlock[0][6] = DZZBlock[27];
+	DQFBlock[0][7] = DZZBlock[28];
+	DQFBlock[1][0] = DZZBlock[2];
+	DQFBlock[1][1] = DZZBlock[4];
+	DQFBlock[1][2] = DZZBlock[7];
+	DQFBlock[1][3] = DZZBlock[13];
+	DQFBlock[1][4] = DZZBlock[16];
+	DQFBlock[1][5] = DZZBlock[26];
+	DQFBlock[1][6] = DZZBlock[29];
+	DQFBlock[1][7] = DZZBlock[42];
+	DQFBlock[2][0] = DZZBlock[3];
+	DQFBlock[2][1] = DZZBlock[8];
+	DQFBlock[2][2] = DZZBlock[12];
+	DQFBlock[2][3] = DZZBlock[17];
+	DQFBlock[2][4] = DZZBlock[25];
+	DQFBlock[2][5] = DZZBlock[30];
+	DQFBlock[2][6] = DZZBlock[41];
+	DQFBlock[2][7] = DZZBlock[43];
+	DQFBlock[3][0] = DZZBlock[9];
+	DQFBlock[3][1] = DZZBlock[11];
+	DQFBlock[3][2] = DZZBlock[18];
+	DQFBlock[3][3] = DZZBlock[24];
+	DQFBlock[3][4] = DZZBlock[31];
+	DQFBlock[3][5] = DZZBlock[40];
+	DQFBlock[3][6] = DZZBlock[44];
+	DQFBlock[3][7] = DZZBlock[53];
+	DQFBlock[4][0] = DZZBlock[10];
+	DQFBlock[4][1] = DZZBlock[19];
+	DQFBlock[4][2] = DZZBlock[23];
+	DQFBlock[4][3] = DZZBlock[32];
+	DQFBlock[4][4] = DZZBlock[39];
+	DQFBlock[4][5] = DZZBlock[45];
+	DQFBlock[4][6] = DZZBlock[52];
+	DQFBlock[4][7] = DZZBlock[54];
+	DQFBlock[5][0] = DZZBlock[20];
+	DQFBlock[5][1] = DZZBlock[22];
+	DQFBlock[5][2] = DZZBlock[33];
+	DQFBlock[5][3] = DZZBlock[38];
+	DQFBlock[5][4] = DZZBlock[46];
+	DQFBlock[5][5] = DZZBlock[51];
+	DQFBlock[5][6] = DZZBlock[55];
+	DQFBlock[5][7] = DZZBlock[60];
+	DQFBlock[6][0] = DZZBlock[21];
+	DQFBlock[6][1] = DZZBlock[34];
+	DQFBlock[6][2] = DZZBlock[37];
+	DQFBlock[6][3] = DZZBlock[47];
+	DQFBlock[6][4] = DZZBlock[50];
+	DQFBlock[6][5] = DZZBlock[56];
+	DQFBlock[6][6] = DZZBlock[59];
+	DQFBlock[6][7] = DZZBlock[61];
+	DQFBlock[7][0] = DZZBlock[35];
+	DQFBlock[7][1] = DZZBlock[36];
+	DQFBlock[7][2] = DZZBlock[48];
+	DQFBlock[7][3] = DZZBlock[49];
+	DQFBlock[7][4] = DZZBlock[57];
+	DQFBlock[7][5] = DZZBlock[58];
+	DQFBlock[7][6] = DZZBlock[62];
+	DQFBlock[7][7] = DZZBlock[63];
+}
+
+void DQuant(int comp) {
+	int i, j;
+
+	for (i = 0; i < 8; i++) {
+		for (j = 0; j < 8; j++) {
+			IDCTBlock[i][j] = (float) (DQFBlock[i][j]
+					* (float) ((comp == 1) ? q_lum[i][j] : q_chrom[i][j]));
+		}
+	}
+}
+
+void IDCT(int comp) {
+
+	int x, y, u, v;
+
+	float32x4_t IDCTvalues;
+
+	float32x4_t IDCTDataBlock;
+
+	float32x4_t temp;
+
+	float32_t sum;
+
+	for (x = 0; x < 8; x++) {
+		for (y = 0; y < 8; y++) {
+			temp = vdupq_n_f32(0.0);
+			for (u = 0; u < 8; u++) {
+				for (v = 0; v < 8; v += 4) {
+					IDCTvalues = vld1q_f32(&IDCTcoeff[x][y][u][v]);
+					IDCTDataBlock = vld1q_f32(&IDCTBlock[u][v]);
+					temp = vmlaq_f32(temp, IDCTvalues, IDCTDataBlock);
+				}
+			}
+
+			sum = vgetq_lane_f32(temp,0) + vgetq_lane_f32(temp,1) +vgetq_lane_f32(temp,2)+vgetq_lane_f32(temp,3);
+
+			dataBlock[x][y] = (int32_t) (sum * 0.25);
+		}
+	}
+
+}
+
+void shift(int comp) {
+	int i, j;
+
+	for (i = 0; i < 8; i++) {
+		for (j = 0; j < 8; j++) {
+			dataBlock[i][j] += 128;
+		}
+	}
+}
+
+void store(int comp) {
+	int i, j;
+	for (i = 0; i < 8; i++) {
+		for (j = 0; j < 8; j++) {
+			YUV[i][j][comp - 1] = dataBlock[i][j];
+		}
+	}
+
+}
+
+void yuv2rgb() {
+	int i, j;
+
+	uint32x4x3_t result;
+
+	float32_t const1 = 1.402;
+	float32_t const2 = 0.344;
+	float32_t const3 = 0.714;
+	float32_t const4 = 1.772;
+
+	float32x4_t shift = vmovq_n_f32(128.0);
+
+
+	for (i = 0; i < 8; i++) {
+		for (j = 0; j < 8; j += 4) {
+
+			float32x4x3_t yuv = vld3q_f32(&YUV[i][j][0]);
+
+			result.val[0] = vcvtq_u32_f32(
+					vaddq_f32(yuv.val[0],
+							vmulq_n_f32(vsubq_f32(yuv.val[2], shift), const1)));
+
+			result.val[1] = vcvtq_u32_f32(
+					vsubq_f32(yuv.val[0],
+							vaddq_f32(
+									vmulq_n_f32(vsubq_f32(yuv.val[1], shift),
+											const2),
+									vmulq_n_f32(vsubq_f32(yuv.val[2], shift),
+											const3))));
+
+			result.val[2] = vcvtq_u32_f32(
+					vaddq_f32(yuv.val[0],
+							vmulq_n_f32(vsubq_f32(yuv.val[1], shift), const4)));
+
+
+			vst3q_u32(&RGB[i][j][0], result);
+
+		}
+
+
+	}
+
+	uint32x4_t max = vdupq_n_u32(255);
+
+	uint32x4x3_t res;
+
+	for (i = 0; i < 8; i++) {
+			for (j = 0; j < 8; j+=4) {
+				uint32x4x3_t clamp = vld3q_u32(&RGB[i][j][0]);
+				res.val[0] = vminq_u32(clamp.val[0], max);
+				res.val[1] = vminq_u32(clamp.val[1], max);
+				res.val[2] = vminq_u32(clamp.val[2], max);
+
+				vst3q_u32(&RGB[i][j][0], res);
+			}
+	}
+
+}
+
+void storeRGB(int x) {
+
+	int i, j, k;
+
+	long tmpx, tmpxx;
+
+	tmpx = (long) x / (512 / 8);
+	tmpxx = (long) x - tmpx * (512 / 8);
+	fseek(fout, (long) (tmpx * (512 / 8) * 192 + tmpxx * 24 + fileoffset),
+	SEEK_SET);
+
+	for (i = 0; i < 8; i++) {
+		for (j = 0; j < 8; j++) {
+			for (k = 0; k < 3; k++) {
+				fwrite(&RGB[i][j][k], sizeof(char), 1, fout);
+			}
+		}
+		fseek(fout, (long) ((512 / 8 - 1) * 24), 1);
+	}
+
+}
+
+void calculateIDCTCoeff() {
+	float PI = 3.14159;
+
+	int x, y, u, v;
+	for (x = 0; x < 8; x++)
+		for (y = 0; y < 8; y++) {
+			for (u = 0; u < 8; u++)
+				for (v = 0; v < 8; v++)
+					IDCTcoeff[x][y][u][v] += ((u == 0) ? 1. / sqrt(2.) : 1.)
+							* ((v == 0) ? 1. / sqrt(2.) : 1.)
+							* cos(
+									((2.0 * (float) x + 1.0) * (float) u * PI)
+											/ 16.0)
+							* cos(
+									((2.0 * (float) y + 1.0) * (float) v * PI)
+											/ 16.0);
+		}
+}
+
+void main(int argc, char * argv[]) {
+
+	unsigned char data1 = 0, data2 = 0, temp = 0;
+
+	int flag, i, j, k, position, comp, counter;
+
+	position = 0;
+
+	if (argc != 2) {
+		printf("usage: decode image_file  (image_file must bi .jpg)\n");
+		exit(0);
+	}
+
+	sprintf(fname, "%s.jpg", argv[1]);
+	if ((fin = fopen(fname, "rb")) == NULL) {
+		printf("Cannot open file: %s\n", fname);
+		exit(0);
+	}
+
+	if ((fp = fopen("h_dc_lum.tbl", "rb")) == NULL) {
+		printf("h_dc_lum.tbl file open error! \n");
+		exit(0);
+	}
+	for (i = 0; i < 12; i++) {
+		fread(&dc_lum[i][0], 2, 1, fp);    /// code length in bits
+		fread(&dc_lum[i][1], 2, 1, fp); /// code word
+	}
+	fclose(fp);
+
+	if ((fp = fopen("h_ac_lum.tbl", "rb")) == NULL) {
+		printf("h_ac_lum.tbl file open error! \n");
+		exit(0);
+	}
+	for (i = 0; i < 16; i++)                              /// i= Run
+		for (j = 0; j < 11; j++) {                        /// j= Size
+			if ((i == 0) || (j > 0) || (i == 15)) {
+				fread(&ac_lum[i][j][0], 2, 1, fp); /// code length
+				fread(&ac_lum[i][j][1], 2, 1, fp); /// codeword
+			}
+		}
+	fclose(fp);
+
+	if ((fp = fopen("h_dc_chrom.tbl", "rb")) == NULL) {
+		printf("h_dc_chrom.tbl file open error! \n");
+		exit(0);
+	}
+	for (i = 0; i < 12; i++) {
+		fread(&dc_chrom[i][0], 2, 1, fp);  /// code length in bits
+		fread(&dc_chrom[i][1], 2, 1, fp); /// code word
+	}
+	fclose(fp);
+
+	if ((fp = fopen("h_ac_chrom.tbl", "rb")) == NULL) {
+		printf("h_ac_chrom.tbl file open error! \n");
+		exit(0);
+	}
+	for (i = 0; i < 16; i++)                              /// i= Run
+		for (j = 0; j < 11; j++) {                        /// j= Size
+			if ((i == 0) || (j > 0) || (i == 15)) {
+				fread(&ac_chrom[i][j][0], 2, 1, fp);   /// code length
+				fread(&ac_chrom[i][j][1], 2, 1, fp);   /// codeword
+			}
+		}
+	fclose(fp);
+
+	calculateIDCTCoeff();
+
+	while (1) {
+		if (data2 == SOS1 && data1 == SOS2) {
+			printf("Skipped header.\n");
+			break;
+		}
+		data2 = data1;
+		data1 = fgetc(fin);
+	}
+
+	fseek(fin, 0x0c, SEEK_CUR);
+
+	j = 0;
+
+	while (1) {
+		if (data2 == EOI1 && data1 == EOI2) {
+			printf("Compressed data has been read.\n");
+			break;
+		}
+		data2 = data1;
+		data1 = fgetc(fin);
+		temp = data1;
+
+		if (data2 == 0xff && data1 == 0) {
+			continue;
+		}
+
+		for (k = 7; k >= 0; k--) {
+			if (temp % 2 == 1) {
+				input[j * 8 + k] = '1';
+			} else {
+				input[j * 8 + k] = '0';
+			}
+
+			temp /= 2;
+		}
+
+		j++;
+	}
+
+	input[j * 8 - 8] = '\0';
+
+	comp = 1;
+
+	counter = 0;
+
+	flag = 1;
+
+	sprintf(fname, "%s.ppm", argv[1]);
+
+	if ((fout = fopen(fname, "wb")) == NULL) {
+		printf("Cannot open .ppm file\n");
+		exit(0);
+	}
+
+	(void) fprintf(fout, "P6\n%d %d\n255\n", 512, 512);
+
+	fileoffset = ftell(fout);
+
+	while (input[position] != '\0' && flag == 1) {
+
+		//Get DC and AC values for blocks
+
+		rl = 0;
+
+		DCvalue[counter] += getDCvalue(&position, comp);
+
+
+		DRLBlock[rl++] = DCvalue[counter];
+
+
+		getACvalue(&position, comp);
+
+
+		DRLE();
+
+		DZigZag();
+
+		DQuant(comp);
+
+		IDCT(comp);
+
+		shift(comp);
+
+		store(comp);
+
+		if (counter == 2) {
+			yuv2rgb();
+
+			storeRGB(count);
+			count++;
+			counter = -1;
+		}
+
+		if (comp == 1) {
+			comp++;
+		} else if (comp == 2) {
+			comp++;
+		} else if (comp > 2) {
+			comp = 1;
+		}
+
+		counter++;
+
+		for (i = 0; i < 9; i++) {
+			if (input[position + i] == '\0') {
+				flag = 0;
+				break;
+			}
+		}
+	}
+
+	fclose(fin);
+	fclose(fout);
+}
